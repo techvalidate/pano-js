68fcb442a1c7e080954d9fb776ff2c1e07a77a07
Matt Lindeboom <mattl@surveymonkey.com>
2018-05-03 18:15:57 -0700

    Removes bad character

--- pano.js
+++ pano.js
@@ -49492,7 +49492,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 exports.default = registerControllers;
 /**
- * registerControllers - A helper function to register imported classes with your stimulus application.å
+ * registerControllers - A helper function to register imported classes with your stimulus application.
  * @param {function} application - your stimulus application instance.
  * @param {hash} controllers - should be a hash with {ControllerName}Controller as the key and the class as the value.
  */

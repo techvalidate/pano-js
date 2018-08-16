dd67bcc1978d03dffe4a13b18c709cd59f90bc95
Matt Lindeboom <mattl@surveymonkey.com>
2018-08-16 11:42:02 -0700

    Update to datepicker start and finish calendar date selections

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import * as UI from './src/utils/ui'
 import Controllers from './src/controllers'
 import * as Sprinkles from './src/sprinkles'
-import DateInput from './src/elements/dateInput'
 import registerControllers from './src/registerControllers'
 
 export {
   Controllers,
-  DateInput,
   registerControllers,
   Sprinkles,
   UI

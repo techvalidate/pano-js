973a83c13520ac33e038ee2e42087d1c45f45655
tjdownes <tj@techvalidate.com>
2019-09-13 11:26:02 -0700

    SOLN-12370 fixes issue when trying to add key drivers to survey

--- src/sprinkles/calendar.js
+++ src/sprinkles/calendar.js
@@ -1,55 +1,56 @@
-UI.click('[show-calendar-modal]', function(e, el) {
-  e.preventDefault();
-  e.stopPropagation();
-  const href = el.attr('href');
+import { default as last } from 'lodash-es/last'
+
+UI.on('click', '[show-calendar-modal]', function(e, el) {
+  e.preventDefault()
+  e.stopPropagation()
+  const href = el.attr('href')
 
   if (href.indexOf('#') === 0) {
-    const data = $(el).data();
+    const data = $(el).data()
 
-    Modals.show($(href), data);
-    return bindCalendars($('#rangestart'), $('#rangeend'), data);
+    Modals.show($(href), data)
+    return bindCalendars($('#rangestart'), $('#rangeend'), data)
   }
-});
+})
 
-
-var bindCalendars = function(rangeStart, rangeEnd, data) {
-  const startCalEl = '.rangestart-value .value';
-  const startCalInput = '#hidden_start_on';
-  const endCalEl = '.rangeend-value .value';
-  const endCalInput = '#hidden_finish_on';
-  const startDate = data['startOn'];
-  const finishDate = data['finishOn'];
+const bindCalendars = function(rangeStart, rangeEnd, data) {
+  const startCalEl = '.rangestart-value .value'
+  const startCalInput = '#hidden_start_on'
+  const endCalEl = '.rangeend-value .value'
+  const endCalInput = '#hidden_finish_on'
+  const startDate = data['startOn']
+  const finishDate = data['finishOn']
 
   const setDateValue = function(el, input, date) {
-    const formattedDate= rome.moment(date).format('MMM D, YYYY');
-    $(el).text(formattedDate);
+    const formattedDate= rome.moment(date).format('MMM D, YYYY')
+    $(el).text(formattedDate)
     if (input) {
-      return $(input).val(date);
+      return $(input).val(date)
     }
-  };
+  }
 
   const startCal = rome(rangeStart[0], {
     dateValidator: rome.val.beforeEq(rangeEnd[0]),
     initialValue: startDate,
     time: false
-  })
-    .on('data', value => setDateValue(startCalEl, startCalInput, value));
+  }).on('data', value => setDateValue(startCalEl, startCalInput, value))
 
   const endCal = rome(rangeEnd[0], {
     dateValidator: rome.val.afterEq(rangeStart[0]),
     time: false,
     initialValue: finishDate
-  }).on('data', value => setDateValue(endCalEl, endCalInput, value));
+  }).on('data', value => setDateValue(endCalEl, endCalInput, value))
 
-  $('.custom_range_form')
-    .on('ajax:complete', () => Modals.close(_.last(Modals.currentModals)))
-    .on('ajax:error', function() {
-      Modals.close(_.last(Modals.currentModals));
-      return alert("Sorry, an error occurred in processing your request. Please contact support if the error persists.");
-    });
+  $('.custom_range_form').ajaxComplete(() => {
+    Modals.close(last(Modals.currentModals))
+  })
+  $('.custom_range_form').ajaxError(() => {
+    Modals.close(last(Modals.currentModals))
+    return alert("Sorry, an error occurred in processing your request. Please contact support if the error persists.")
+  })
 
-  startCal.refresh();
-  endCal.refresh();
-  setDateValue(startCalEl, null, startCal.getDate());
-  return setDateValue(endCalEl, null, endCal.getDate());
-};
+  startCal.refresh()
+  endCal.refresh()
+  setDateValue(startCalEl, null, startCal.getDate())
+  return setDateValue(endCalEl, null, endCal.getDate())
+}
--- src/sprinkles/forms.js
+++ src/sprinkles/forms.js
@@ -1,43 +1,37 @@
 import { fire } from 'delegated-events'
+require('jquery-ujs')
 // =====================================================
 // links that submit containing form on click
 // =====================================================
 
-UI.click('form .js-submit-on-click', function(e, el) {
-  let f;
-  if (f = el.closest('form')) {
-    e.stopPropagation();
-    return f.submit();
-  }
-});
+UI.click('form .js-submit-on-click', (e, el) => {
+  e.preventDefault()
+  e.stopPropagation()
+  $('#add-key-drivers-form').trigger('submit')
+})
 
-UI.click('form .js-submit-on-click-with-propagation', function(e, el) {
-  let f;
-  if (f = el.closest('form')) {
-    // Fire native submit on form element for RailsUjs to capture
-    fire(f[0], 'submit')
-  }
-  return true
-});
+UI.click('form .js-submit-on-click-with-propagation', (e, el) => {
+  const form = el.closest('form')
+  e.preventDefault()
+  e.stopPropagation()
+  // Fire native submit on form element for RailsUjs to capture
+  fire(form[0], 'submit')
+})
 
 // =====================================================
 // fields that auto-focus on page load
 // =====================================================
-
-UI.load(() => $('input.js-focus-on-load').focus());
-
+UI.load(() => $('input.js-focus-on-load').focus())
 
 // =====================================================
 // validate all forms with jquery validation plugin.
 // =====================================================
-
-UI.load(() => $.bindFormValidation($('body')));
-
+UI.load(() => $.bindFormValidation($('body')))
 
 $.bindFormValidation = html =>
-//add form validation
-  html.find('form').each(function() {
-    const f = $(this);
+  //add form validation
+  html.find('form').each(() => {
+    const f = $(this)
 
     if (!f.hasClass('js-novalidate')) {
       return f.validate({
@@ -47,41 +41,39 @@ $.bindFormValidation = html =>
           $(el).parent().addClass(errorClass)
           $(el).parent().siblings('.form-helper-text').hide()
         },
-        unhighlight: function(el, errorClass, validClass) {
+        unhighlight: (el, errorClass, validClass) => {
           $(el).removeClass(errorClass).addClass(validClass)
           $(el).parent().removeClass(errorClass)
           $(el).parent().siblings('.form-helper-text').show()
         }
-      });
+      })
     }
   })
 
 // =====================================================
 // for custom password validation message display
 // =====================================================
-$.passwordComplexityValidationMessage = function(element) {
-
-  const value = $(element).val();
+$.passwordComplexityValidationMessage = (element) => {
+  const value = $(element).val()
 
   if ((value.length >= 8) && $('.min-chars')) {
-    $('.min-chars').addClass('valid');
+    $('.min-chars').addClass('valid')
   } else {
-    $('.min-chars').removeClass('valid');
+    $('.min-chars').removeClass('valid')
   }
 
   if (/\d/.test(value) && $('.min-nums')) {
-    $('.min-nums').addClass('valid');
+    $('.min-nums').addClass('valid')
   } else {
-    $('.min-nums').removeClass('valid');
+    $('.min-nums').removeClass('valid')
   }
 
   if (value.match(/[a-z]/i) && $('.min-letters')) {
-    $('.min-letters').addClass('valid');
+    $('.min-letters').addClass('valid')
   } else {
-    $('.min-letters').removeClass('valid');
+    $('.min-letters').removeClass('valid')
   }
-};
-
+}
 
 // =====================================================
 // custom validator rules
@@ -91,77 +83,77 @@ $.passwordComplexityValidationMessage = function(element) {
 // custom rule: string contains at least one integer, one alpha character and is at least 8 characters in length
 // {data: {rule_passwordComplexityMatch: 'passwordComplexityMatch'}}
 $.validator.addMethod('passwordComplexityMatch', value => /[a-z].*[0-9]|[0-9].*[a-z]/i.test(value) && (value.length >= 8)
-, '8 characters minimum, 1 number and 1 letter');
-
+, '8 characters minimum, 1 number and 1 letter')
 
 // ======== dateRangeValidate ======== #
 // custom rule: Validate that the date is before the field specified by the rule attribute
 // {data: {rule_dateRangeValidate: 'my_end_date_field'}}
-$.validator.addMethod('dateRangeValidate', function(value, el) {
-  let isValid = true;
+$.validator.addMethod('dateRangeValidate', (value, el) => {
+  let isValid = true
+
   // validate date ranges
-  const dtField = $(el).attr('data-rule-dateRangeValidate');
-  const compareDate = moment(new Date($(dtField).val()));
+  const dtField = $(el).attr('data-rule-dateRangeValidate')
+  const compareDate = moment(new Date($(dtField).val()))
 
   // for date only pickers, we need to set the time to the current
   // time if the selected date is today, for validation purposes.
   // add 20 minutes so the user doesn't get stuck if they allow the page to sit
-  const newValue = moment(new Date(value));
-  const currentDate = moment(new Date());
+  const newValue = moment(new Date(value))
+  const currentDate = moment(new Date())
 
   if (($(el).attr('data-datepicker') === 'date-only') && (newValue.diff(currentDate, 'days') === 0)) {
     newValue.set({
       'hour' : currentDate.get('hour'),
       'minute'  : currentDate.get('minute') + 20,
       'second' : currentDate.get('second')
-    });
+    })
     compareDate.set({
       'hour' : currentDate.get('hour'),
       'minute'  : currentDate.get('minute') + 20,
       'second' : currentDate.get('second')
-    });
+    })
   }
 
   if (compareDate) {
-    isValid = newValue.isSameOrBefore(compareDate);
+    isValid = newValue.isSameOrBefore(compareDate)
   }
 
-  return isValid;
+  return isValid
 }
-, 'Start must come before End');
+, 'Start must come before End')
 
 // ======== dateIsInFuture ======== #
 // custom rule: Validate that the date is in the future
 // {data: {rule_dateIsInFuture: 'dateIsInFuture'}}
-$.validator.addMethod('dateIsInFuture', function(value, el) {
+$.validator.addMethod('dateIsInFuture', (value, el) => {
   // for date only pickers, we need to set the time to the current
   // time if the selected date is today, for validation purposes.
   // add 20 minutes so the user doesn't get stuck if the allow the page to sit
-  const newValue = moment(new Date(value));
-  const currentDate = moment(new Date());
+  const newValue = moment(new Date(value))
+  const currentDate = moment(new Date())
 
   if (($(el).attr('data-datepicker') === 'date-only') && (newValue.diff(currentDate, 'days') === 0)) {
     newValue.set({
       'hour' : currentDate.get('hour'),
       'minute'  : currentDate.get('minute') + 20,
       'second' : currentDate.get('second')
-    });
+    })
   }
-  return newValue.isSameOrAfter(currentDate);
+  return newValue.isSameOrAfter(currentDate)
 }
-, 'Current or future date required');
+, 'Current or future date required')
 
 // ======== customRomeValidators ======== #
 // These are executed independently of jquery Validate
 // used to validate Rome fields, if needed
 // TODO: maybe find a better way to abstract these rules
-$.customRomeValidators = function(field) {
+$.customRomeValidators = (field) => {
   // force jquery validate to check the field to clear any invalid status
-  $(field.associated).valid();
+  $(field.associated).valid()
 
   // validate a related field, if needed
-  const relatedField = $(field.associated).attr('data-validate-related');
+  const relatedField = $(field.associated).attr('data-validate-related')
   if (relatedField) {
-    return $(relatedField).valid();
+    return $(relatedField).valid()
   }
-};
+}
